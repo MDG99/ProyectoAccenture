9bc269dd87e03ba1a29a672e3f420ec717bedf3f
IgnacioIsaac <ignacioisaac30@gmail.com>
2022-12-14 07:17:01 -0600

    Simple updates

--- FinalApp/src/main/java/com/ModernCloud/Entity/Geography.java
+++
@@ -1,8 +0,0 @@
-package com.ModernCloud.Entity;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Data;
-
-@Data
-public class Geography {
-
-}
--- FinalApp/src/main/java/com/ModernCloud/FinalApp/FinalAppApplication.java
+++ FinalApp/src/main/java/com/ModernCloud/FinalApp/FinalAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FinalAppApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FinalAppApplication.class, args);
 	}
